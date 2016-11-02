5aff7da8f44a0214cf86b5c6110548984916ec51
Pushpinder Singh Grewal <Pndrgrewal12@gmail.com>
2016-11-02 18:16:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
 var articleOne =
 { title: 'Article one',
   heading:  'Article one',
@@ -13,40 +14,51 @@ var articleOne =
     
     
 };
+
+function createTemplate(data){
+  var title=data.title;
+  var date=data.date;
+  var heading=data.heading;
+  var content=data.content;
+  
+
+
 var htmlTemplate=`<!Doctype html>
 <html>
-  <head>
-    <link href="/ui/style.css" rel="stylesheet" />
-   
-    <title>
-    ${title}
-    </title>
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    
-  </head>
-  <body>
-    <div class="container">
-      <div>
-        <a href="/">Home </a>
-      </div>  
-      <hr/>
-      <h3>${heading}</h3>
-      <div>
-      ${date}   
-      </div>    
-      <div>
-      ${content }  
-      </div>
-    </div>
-  </body>
+<head>
+<link href="/ui/style.css" rel="stylesheet" />
+
+<title>
+${title}
+</title>
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+
+</head>
+<body>
+<div class="container">
+<div>
+<a href="/">Home </a>
+</div>  
+<hr/>
+<h3>${heading}</h3>
+<div>
+${date}   
+</div>    
+<div>
+${content }  
+</div>
+</div>
+</body>
 </html>  
 `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {

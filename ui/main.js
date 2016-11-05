efe009eef554f2d6e57775d1df3ab67496afac70
Pushpinder Singh Grewal <Pndrgrewal12@gmail.com>
2016-11-05 11:00:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="New value";
+var gyanni=document.getElementById('gyanni');
+gyanni.onclick=function()
+{
+    gyanni.style.marginLeft='10px';
+    
+    
+}

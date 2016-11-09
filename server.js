937234dfe65b55aba56fa2b3219c4e7f9fe7d09b
Pushpinder Singh Grewal <Pndrgrewal12@gmail.com>
2016-11-09 13:28:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,17 @@ app.get('/ui/pinder.jpg', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+var names=[];
+app.get('/submit-name',function(req,res){//URl:/submit-name?name=xxxx
+    //Get the name from the request
+    var name=req.query.name;
+    names.push(name);
+    //JSON; javacript object notation
+    res.send(JSON.stringify(names));
+    
+    
+    
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80

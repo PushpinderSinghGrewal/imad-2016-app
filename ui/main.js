0b22e840d1ef80eba67759aeb2fa292ab355b3ad
Pushpinder Singh Grewal <Pndrgrewal12@gmail.com>
2016-11-25 20:37:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 function loadLoginForm () {
     var loginHtml = `
-        <h3>Login/Register to unlock awesome features</h3>
+        <h3>You can Login/Register here</h3>
         <input type="text" id="username" placeholder="username" />
         <input type="password" id="password" />
         <br/><br/>
